20e9697b31d73e2bb4f90eaef49c9a45ef241fb2
ArturYaniuk <pobedutelb@gmail.com>
2024-12-12 18:41:17 +0200

    Add project files.

---
+++ Intermediate/Build/Win64/UnrealEditor/Inc/MidnightTask/UHT/MidnightTaskGameMode.generated.h
@@ -0,0 +1,54 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "MidnightTaskGameMode.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef MIDNIGHTTASK_MidnightTaskGameMode_generated_h
+#error "MidnightTaskGameMode.generated.h already included, missing '#pragma once' in MidnightTaskGameMode.h"
+#endif
+#define MIDNIGHTTASK_MidnightTaskGameMode_generated_h
+
+#define FID_Files_My_Works_UE_works_MidnightTask_Source_MidnightTask_MidnightTaskGameMode_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAMidnightTaskGameMode(); \
+	friend struct Z_Construct_UClass_AMidnightTaskGameMode_Statics; \
+public: \
+	DECLARE_CLASS(AMidnightTaskGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/MidnightTask"), MIDNIGHTTASK_API) \
+	DECLARE_SERIALIZER(AMidnightTaskGameMode)
+
+
+#define FID_Files_My_Works_UE_works_MidnightTask_Source_MidnightTask_MidnightTaskGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	AMidnightTaskGameMode(AMidnightTaskGameMode&&); \
+	AMidnightTaskGameMode(const AMidnightTaskGameMode&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(MIDNIGHTTASK_API, AMidnightTaskGameMode); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMidnightTaskGameMode); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AMidnightTaskGameMode) \
+	MIDNIGHTTASK_API virtual ~AMidnightTaskGameMode();
+
+
+#define FID_Files_My_Works_UE_works_MidnightTask_Source_MidnightTask_MidnightTaskGameMode_h_9_PROLOG
+#define FID_Files_My_Works_UE_works_MidnightTask_Source_MidnightTask_MidnightTaskGameMode_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Files_My_Works_UE_works_MidnightTask_Source_MidnightTask_MidnightTaskGameMode_h_12_INCLASS_NO_PURE_DECLS \
+	FID_Files_My_Works_UE_works_MidnightTask_Source_MidnightTask_MidnightTaskGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> MIDNIGHTTASK_API UClass* StaticClass<class AMidnightTaskGameMode>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Files_My_Works_UE_works_MidnightTask_Source_MidnightTask_MidnightTaskGameMode_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Source/MidnightTask/MidnightTaskGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MidnightTaskGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMidnightTaskGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMidnightTaskGameMode();
+};
+
+
+
